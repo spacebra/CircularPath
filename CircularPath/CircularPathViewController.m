64871b8f2b1215e5d8ca761980f67e44a1e62e95
Piano <pianooooooooo@gmail.com>
2012-04-25 16:16:23 +0700

    Initial Commit

---
+++ CircularPath/CircularPathViewController.m
@@ -0,0 +1,34 @@
+//
+//  CircularPathViewController.m
+//  CircularPath
+//
+//  Created by  on 4/25/55 BE.
+//  Copyright (c) 2555 __MyCompanyName__. All rights reserved.
+//
+
+#import "CircularPathViewController.h"
+
+@interface CircularPathViewController ()
+
+@end
+
+@implementation CircularPathViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
